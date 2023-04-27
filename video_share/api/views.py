ff3a80667bfb45efb3dad331dffed4d6f9e8ab63
patrykjamroz <jamroz.patryk@gmail.com>
2023-04-27 22:53:03 +0200

    add and configure simple JWT

--- video_share/api/views.py
+++ video_share/api/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +8,15 @@ from django.shortcuts import render
 
 def main(request):
     return HttpResponse('Hello')
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
