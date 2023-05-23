c754fb78a39b6aa61b47a64a007dcb1134bf4a97
patrykjamroz <jamroz.patryk@gmail.com>
2023-05-23 20:41:41 +0200

    #9 add app menu

--- video_share/api/serializers.py
+++ video_share/api/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework.serializers import ModelSerializer
-from .models import PostDraft
+from .models import PostDraft, Profile
 
 
 class PostDraftSerializer(ModelSerializer):
     class Meta:
         model = PostDraft
         fields = '__all__'
+
+class ProfileSerializer(ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+    def get_following(self, obj):
+        return obj.following.values_list('username', flat=True)
+
+    def get_followers(self, obj):
+        return obj.user.followers.values_list('username', flat=True)
--- video_share/frontend/src/components/AppMenu.tsx
+++ video_share/frontend/src/components/AppMenu.tsx
@@ -1,22 +1,84 @@
-import { Avatar, Box, Button, Heading } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import * as React from "react";
 import { useAuthContext } from "../context/AuthProvider";
+import { AddIcon, HamburgerIcon } from "@chakra-ui/icons";
+import { axiosInstance } from "../api/axiosInstance";
 
 export function AppMenu() {
   const authContext = useAuthContext();
 
+  const [profile, setProfile] = React.useState(null);
+
+  React.useEffect(() => {
+    axiosInstance.get("/profile/").then((res) => setProfile(res.data));
+  }, []);
+
   return (
-    <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Heading>Heding</Heading>
-      <Box display={"flex"} flexDir="row">
-        <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
-        <Heading as="h4" size="md">
-          {authContext.user.username}
+    <Container centerContent>
+      <Flex
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        alignItems={"center"}
+        flexDir="column"
+        width="100%"
+        mt={5}
+        // border={"pink 1px dashed"}
+      >
+        <Heading textAlign="center" mb={"1.5rem"}>
+          Video Share
         </Heading>
-      </Box>
-      <Button colorScheme="blue" onClick={authContext.logoutUser}>
-        Log out
-      </Button>
-    </Box>
+        <Flex alignItems="center" flexDir="column">
+          <Avatar
+            size={"xl"}
+            name="Dan Abrahmov"
+            src="https://bit.ly/dan-abramov"
+            mb="0.5rem"
+          />
+          <Heading as="h4" size="lg">
+            {authContext.user.username}
+          </Heading>
+          <Text fontSize="md">In love with React & Next</Text>
+          <Text fontSize="sm">Posts: {profile?.post_count ?? 0}</Text>
+          <Text fontSize="sm">Following: {profile?.following.length ?? 0}</Text>
+          <Text fontSize="sm">Followers: {profile?.followers.length ?? 0}</Text>
+        </Flex>
+        <Button
+          leftIcon={<HamburgerIcon boxSize={5} />}
+          size={"lg"}
+          colorScheme="teal"
+          variant="link"
+          mt="1rem"
+        >
+          Home
+        </Button>
+        <Button
+          size={"lg"}
+          leftIcon={<AddIcon boxSize={4} />}
+          colorScheme="teal"
+          variant="link"
+          mt="1rem"
+        >
+          Create
+        </Button>
+        <Button
+          size="sm"
+          colorScheme="blue"
+          mt="1.5rem"
+          width={20}
+          onClick={authContext.logoutUser}
+        >
+          Log out
+        </Button>
+      </Flex>
+    </Container>
   );
 }
